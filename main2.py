c262a4445ecc69ec791284ce520864ac20f21da7
khw11044 <khw11044@naver.com>
2024-09-24 16:37:10 +0900

    first commit

---
+++ main2.py
@@ -0,0 +1,91 @@
+import streamlit as st
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import ChatMessage
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import load_prompt
+import glob
+
+# prompt-maker 
+# https://smith.langchain.com/hub/hardkothari/prompt-maker?organizationId=88007055-284e-585d-8633-daa8efb5ee48
+
+
+st.set_page_config(page_title="나만의 ChatGPT 💬", page_icon="💬")
+st.title("나만의 ChatGPT 💬")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+prompt_input = None
+# 사이드바 생성 
+with st.sidebar:
+    clear_btn = st.button("대화내용 초기화")
+    prompt_files = glob.glob("prompts/*.yaml")
+    selected_prompt = st.selectbox("프롬프트를 선택해 주세요.", prompt_files, index=0)
+    task_input = st.text_input("Task 입력 \n 예시: 블로그 글 작성 ", "")
+    
+    if task_input:
+        prompt_input = st.text_input("프롬프트 입력", "")
+
+
+    
+def print_messages():
+    for chat_message in st.session_state["messages"]:
+        st.chat_message(chat_message.role).write(chat_message.content)
+
+def add_message(role, message):
+    st.session_state["messages"].append(ChatMessage(role=role, content=message))
+
+
+# 체인을 생성합니다.
+def create_chain(prompt_file_path, task=""):
+    prompt = load_prompt(prompt_file_path, encoding="utf-8")
+    
+    if task:
+        prompt = prompt.partial(task=task)
+    
+    llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+    chain = prompt | llm | StrOutputParser()
+    return chain
+
+
+# 초기화 버튼 
+if clear_btn:
+    retriever = st.session_state["messages"].clear()
+
+# 이전 대화 기록 출력 
+print_messages()
+
+# 만약 프롬프트를 만들어 달라고 하면
+if prompt_input:
+    chain = create_chain(selected_prompt, task=task_input)  
+    # 스트리밍 호출 
+    response = chain.stream({"question": prompt_input})
+    with st.chat_message("assistant"):
+        container = st.empty()
+        
+        ai_answer = ""
+        for token in response:
+            ai_answer += token
+            container.markdown(ai_answer)
+
+# 만약 사용자 입력이 들어오면 
+if user_input := st.chat_input():
+    # 사용자 입력 
+    st.chat_message("user").write(user_input)
+    chain = create_chain(selected_prompt, task=task_input)    
+    
+    # 스트리밍 호출 
+    response = chain.stream({"question": user_input})
+    with st.chat_message("assistant"):
+        container = st.empty()
+        
+        ai_answer = ""
+        for token in response:
+            ai_answer += token
+            container.markdown(ai_answer)
+
+    # 대화 기록을 저장한다.
+    add_message("user", user_input)
+    add_message("assistant", ai_answer)
+    
